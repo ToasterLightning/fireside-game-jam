322c4430599e28e012ba365b76599154cf928a51
AprilSR <aprilkopec47@gmail.com>
2025-04-27 01:51:22 -0700

    move :3

--- player.gd
+++ player.gd
@@ -2,21 +2,25 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+const TURN_RATE = 5.0
 
 func _physics_process(delta: float) -> void:
 	#Get input direction
-	var direction = Vector2.ZERO
-	if Input.is_action_pressed("up"):
-		direction += Vector2.UP
-	if Input.is_action_pressed("left"):
-		direction += Vector2.LEFT
+	var mov_direction = 0
+	var turn_direction = 0
+	
 	if Input.is_action_pressed("right"):
-		direction += Vector2.RIGHT
-	if Input.is_action_pressed("down"):
-		direction += Vector2.DOWN
-		
-	#Design choice whether or not to make diagnoals faster
-	#direction = direction.normalized()
-	velocity = direction * SPEED
+		if not Input.is_action_pressed("left"):
+			turn_direction = 1
+	elif Input.is_action_pressed("left"):
+		turn_direction = -1
+	elif Input.is_action_pressed("up"):
+		if not Input.is_action_pressed("down"):
+			mov_direction = 1
+	elif Input.is_action_pressed("down"):
+		mov_direction = -1
+	
+	
+	rotate(TURN_RATE * turn_direction * delta)
+	velocity = mov_direction * SPEED * Vector2(sin(rotation),-cos(rotation))
 	move_and_slide()
